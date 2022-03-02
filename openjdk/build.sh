a829efe6ac99daf3e482a9ce1b2332cf79441d87
thevirtuoso1973 <chris.tomy@hotmail.com>
2022-03-02 11:09:12 +0000

    remove binaries after build

--- openjdk/build.sh
+++ openjdk/build.sh
@@ -7,7 +7,7 @@ fdfind Dockerfile$ ./binary_dockerfiles | sed "s/.Dockerfile//" | parallel --bar
 
 echo Extract the extracted binaries by starting a container.
 # Could do this in parallel but I doubt much perf improvement.
-mkdir ./binaries -p
+mkdir ./binaries
 for i in $(fdfind Dockerfile$ ./binary_dockerfiles | sed "s/.Dockerfile//")
 do
     docker container create --name extract$$ $i
@@ -17,3 +17,5 @@ done
 
 echo Finally, build the generated Dockerfiles, they should use the binaries dir, which should be present in their build context.
 fdfind Dockerfile$ | rg -v '(binary_dockerfiles|windows)' | parallel --bar docker build . -f {}
+
+rm -rf ./binaries
