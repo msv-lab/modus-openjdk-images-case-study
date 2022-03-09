cfb2be52c3e1b6983e402cc5e0feab3e0a02e622
Chris <46009390+thevirtuoso1973@users.noreply.github.com>
2022-03-09 20:24:29 +0000

    Compare image size script (#16)
    * Create main.yml
    
    * Update main.yml
    
    * add step to tag images
    
    * add direct image size comparison
    
    * use container-diff
    
    * replace with sudo install
    
    * replace yaml syntax
    
    * just diff on file size since logs are large
    
    * add another job that should only output comparison
    
    * use sed/awk to subtract
    
    * replace single quote with double quote
    
    * remove one of the jobs

---
+++ benchmark-scripts/image_compare.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+modus build ./openjdk-images-case-study -f <(cat ./openjdk-images-case-study/*.Modusfile) --json > build.json;
+jq '.[] | [.digest, (.args | join("-"))] | join(" ")' build.json | xargs -I % sh -c 'docker tag %';
+jq '.[].args | join("-")' build.json | xargs -I % sh -c "container-diff diff openjdk:% daemon://% | sed -n 6p | awk '{print $2 - $1;}'";
