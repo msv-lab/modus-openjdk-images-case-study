8e80891aa9b8f300bac77622461ec25d5cd6aabd
Chris <chris.tomy.19@ucl.ac.uk>
2022-03-10 20:39:29 +0000

    add image comparison script

--- benchmark-scripts/image_compare.sh
+++ benchmark-scripts/image_compare.sh
@@ -2,4 +2,5 @@
 
 modus build ./openjdk-images-case-study -f <(cat ./openjdk-images-case-study/*.Modusfile) 'openjdk(A, B, C)' --json > build.json;
 jq '.[] | [.digest, (.args | join("-"))] | join(" ")' build.json | xargs -I % sh -c 'docker tag %';
-jq '.[].args | join("-")' build.json | xargs -I % sh -c "container-diff diff openjdk:% daemon://%" | sed -n 6p | awk '{print $2 - $1;}';
+jq '.[].args | join("-")' build.json | xargs -I % sh -c "echo -n '% '; docker image inspect % | jq .[].Size -j; echo -n ' '; docker image inspect openjdk:% | jq .[].Size" > compare.txt;
+cat compare.txt | awk '{print $1, ($2 - $3 > 0 ? $2 - $3 : $3 - $2);}' | sort -n -k2;
