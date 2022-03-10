f702a33525eed468b5d9910fb9c58f043e2ea91f
thevirtuoso1973 <chris.tomy@hotmail.com>
2022-03-10 01:01:10 +0000

    fix image compare script

--- benchmark-scripts/image_compare.sh
+++ benchmark-scripts/image_compare.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-modus build ./openjdk-images-case-study -f <(cat ./openjdk-images-case-study/*.Modusfile) --json > build.json;
+modus build ./openjdk-images-case-study -f <(cat ./openjdk-images-case-study/*.Modusfile) 'openjdk(A, B, C)' --json > build.json;
 jq '.[] | [.digest, (.args | join("-"))] | join(" ")' build.json | xargs -I % sh -c 'docker tag %';
-jq '.[].args | join("-")' build.json | xargs -I % sh -c "container-diff diff openjdk:% daemon://% | sed -n 6p | awk '{print $2 - $1;}'";
+jq '.[].args | join("-")' build.json | xargs -I % sh -c "container-diff diff openjdk:% daemon://%" | sed -n 6p | awk '{print $2 - $1;}';
