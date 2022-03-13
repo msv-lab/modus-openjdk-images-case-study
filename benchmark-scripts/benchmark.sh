2a021929d38e1d5171cdad4b4f56c78de7effbaa
thevirtuoso1973 <chris.tomy@hotmail.com>
2022-03-13 12:42:02 +0000

    update benchmark.sh

--- benchmark-scripts/benchmark.sh
+++ benchmark-scripts/benchmark.sh
@@ -14,16 +14,21 @@ cd openjdk
 ./update.sh
 cd ../../
 
-docker builder prune -a -f && docker image prune -a -f;
-/usr/bin/time -o benchmarks/modus-time.log -a -p modus build ./openjdk-images-case-study 'openjdk(A, B, C)' -f <(cat ./openjdk-images-case-study/*.Modusfile) --image-export-concurrency=4;
+for i in {1..10}
+do
+    docker builder prune -a -f && docker image prune -a -f;
+    /usr/bin/time -o benchmarks/modus-time.log -a -p modus build ./openjdk-images-case-study 'openjdk(A, B, C)' -f <(cat ./openjdk-images-case-study/*.Modusfile) \
+        --image-export-concurrency=8 \
+        --image-resolve-concurrency=8;
 
-docker builder prune -a -f && docker image prune -a -f;
-fdfind Dockerfile$ ./openjdk | rg -v windows | /usr/bin/time -o benchmarks/official.log -a -p xargs -I % sh -c 'docker build . -f %';
+    docker builder prune -a -f && docker image prune -a -f;
+    fdfind Dockerfile$ ./openjdk | rg -v windows | /usr/bin/time -o benchmarks/official.log -a -p xargs -I % sh -c 'docker build ./openjdk -f %';
 
-docker builder prune -a -f && docker image prune -a -f;
-fdfind Dockerfile$ ./openjdk | rg -v windows | /usr/bin/time -o benchmarks/official-parallel.log -a -p parallel --bar docker build . -f {};
+    docker builder prune -a -f && docker image prune -a -f;
+    fdfind Dockerfile$ ./openjdk | rg -v windows | /usr/bin/time -o benchmarks/official-parallel.log -a -p parallel --bar docker build ./openjdk -f {};
 
-docker builder prune -a -f && docker image prune -a -f;
-cd ./openjdk-images-case-study/openjdk
-/usr/bin/time -o ../../benchmarks/official-optimized.log -a -p ./build.sh
-cd -
+    docker builder prune -a -f && docker image prune -a -f;
+    cd ./openjdk-images-case-study/openjdk
+    /usr/bin/time -o ../../benchmarks/official-optimized.log -a -p ./build.sh
+    cd -
+done
